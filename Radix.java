905f4ed73041f42881d148405ab9b0c4ec48cb93
Tammy <tchen00@stuy.edu>
2019-04-03 17:38:34 -0400

    add first pass of radix

--- Radix.java
+++ Radix.java
@@ -9,9 +9,26 @@ public class Radix{
     for (int i = 0; i < 20; i++){
       buckets[i] = new MyLinkedList<>();
     }
+    MyLinkedList<Integer> list = new MyLinkedList<Integer>(); //initializing new linked list
+    int digits = max(data); //number of digits in max value of data
+
+    for (int i = 0; i < digits; i++){ //loops through number of passes
+      // first pass
+      if (i == 0){
+        for (int j = 0; j < data.length; j++){
+          int temp = data[j];
+          // abs of digit
+          int idx = Math.abs((int)(temp / (Math.pow(10, i))) % 10);
+          // if positive
+          if (temp >= 0) buckets[idx + 10].add(temp);
+          // if negative (add in inverted order -- from Mr. K in class)
+          else buckets[9 - idx].add(temp);
+        }
+      }
+    }
   }
 
-  // finding the max
+  // finding the max number of digits
   public static int max(int[] data){
     if (data.length == 0){
       // if the length is 0 obv return 0

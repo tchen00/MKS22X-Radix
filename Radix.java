570420156049546c6045c6a3bbfc1a4ae76a9d97
Tammy <tchen00@stuy.edu>
2019-04-03 17:28:31 -0400

    add max method

--- Radix.java
+++ Radix.java
@@ -11,6 +11,24 @@ public class Radix{
     }
   }
 
+  // finding the max
+  public static int max(int[] data){
+    if (data.length == 0){
+      // if the length is 0 obv return 0
+      return 0;
+    }
+    // setting a temp value to store
+    int temp = Math.abs(data[0]);
+    // looping through data
+    for (int i = 0; i < data.length; i++){
+      // if next is greater than current temp -- replace
+      if (Math.abs(data[i]) > temp){
+        temp = Math.abs(data[i]);
+      }
+     }
+    return (int)Math.log10(temp) + 1;
+  }
+
   public static void main(String[] args){
 
   }

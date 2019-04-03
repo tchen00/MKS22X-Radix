35da2e27cc7f82fadb44e5716a42cfe53a6d0237
Tammy <tchen00@stuy.edu>
2019-04-03 17:21:41 -0400

    finished copying everything into buckets

--- Radix.java
+++ Radix.java
@@ -3,7 +3,12 @@ import java.util.*;
 
 public class Radix{
   public static void radixsort(int[]data){
-
+    //initializing mylinkedlist
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    //putting info into buckets
+    for (int i = 0; i < 20; i++){
+      buckets[i] = new MyLinkedList<>();
+    }
   }
 
   public static void main(String[] args){
